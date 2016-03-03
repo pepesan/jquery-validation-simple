2435a8529742835099c2cc5bc12c3d09617e457d
David Vaquero Santiago <pepesan@gmail.com>
2016-03-03 13:26:04 +0100

    adaptaciones de datepicker al castellano

--- js/index-jquery-ui-datepicker.js
+++ js/index-jquery-ui-datepicker.js
@@ -2,14 +2,37 @@
  * Created by pepesan on 3/3/16.
  */
 function init(){
-    $( "#datepicker" ).datepicker();
-    //$( "#datepicker" ).datepicker( "option", "showAnim", "fadeIn" );
-
+    //$( "#datepicker" ).datepicker();
+    $( "#datepicker" ).datepicker({
+        showAnim: "fold"
+    });
+    //$( "#datepicker" ).datepicker( "option", "dateFormat", "dd/mm/yy" );
+    /*
     function anima() {
         $( "#datepicker" ).datepicker( "option", "showAnim", $(this).val() );
     }
     $( "#anim" ).change(anima);
+    */
 
+
+    $.datepicker.regional['es'] = {
+        closeText: 'Cerrar',
+        prevText: '<Ant',
+        nextText: 'Sig>',
+        currentText: 'Hoy',
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+        dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+        weekHeader: 'Sm',
+        dateFormat: 'dd/mm/yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['es']);
 
 }
 
