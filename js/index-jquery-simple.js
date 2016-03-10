0bb05dc43a0d323e48c35e49b37dfbe17da8a029
David Vaquero Santiago <pepesan@gmail.com>
2016-03-10 10:44:31 +0100

    ejemplo  con un objeto app

--- js/index-jquery-simple.js
+++ js/index-jquery-simple.js
@@ -1,10 +1,13 @@
 /**
  * Created by pepesan on 10/3/16.
  */
-function pulsado (){
-    $("p").hide();
-}
-function init(){
-    $("button").click(pulsado);
-}
-$(document).ready(init);
+var app={
+    pulsado:function  (){
+        $("p").hide();
+    },
+    init:function (){
+        $("button").click(app.pulsado);
+    }
+};
+
+$(document).ready(app.init);
