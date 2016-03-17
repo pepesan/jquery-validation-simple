e3cbb235c1dff6662cb604a84ce1dd0946d7b1c6
David Vaquero Santiago <pepesan@gmail.com>
2016-03-17 10:57:34 +0100

    ejemplos de validacion de un campo, eventos focus y blur

---
+++ js/index-jquery-eventos2.js
@@ -0,0 +1,32 @@
+/**
+ * Created by pepesan on 10/3/16.
+ */
+//función inicial cuando carga la página
+//cualquier modificacióin de DOM debe ir aquí dentro {...}
+
+/*
+ Este código de aquí es igual al de abajo entero
+ $(function() {
+    console.log("Página cargada");
+ });
+ */
+var app={
+    activate: function () {
+        //console.log("Evento OnBlur");
+        $("#campo").toggleClass("active");
+    },
+    init:function (){
+        console.log("Página cargada");
+        $("#campo").focus(app.activate);
+        $("#campo").blur(app.activate);
+    },
+    loaded:function (){
+    console.log("Página completamente cargada");
+}
+};
+
+//cuando se carga la página ejecuta la función init()
+$(document).ready(app.init);
+// esto permite definir un evento cuando la página está cargada completamente
+
+$(window).load(app.loaded);
