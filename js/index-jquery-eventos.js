3f108386f4073c4092cb15e612a90508cb61ccfc
David Vaquero Santiago <pepesan@gmail.com>
2016-03-17 09:56:03 +0100

    ejemplos de eventos basicos con jquery con estructura de objeto app

--- js/index-jquery-eventos.js
+++ js/index-jquery-eventos.js
@@ -10,18 +10,21 @@
     console.log("Página cargada");
  });
  */
-function clickado(){
-    console.log("Me han pulsado");
-}
-function init(){
-    console.log("Página cargada");
-    $("#boton").click(clickado);
-}
-function loaded(){
-    console.log("Página completamente cargada");
-}
+var app={
+    clickado:function (){
+        console.log("Me han pulsado");
+    },
+    init:function (){
+        console.log("Página cargada");
+        $("#boton").click(app.clickado);
+    },
+    loaded:function (){
+        console.log("Página completamente cargada");
+    }
+};
+
 //cuando se carga la página ejecuta la función init()
-$(document).ready(init);
+$(document).ready(app.init);
 // esto permite definir un evento cuando la página está cargada completamente
 
-$(window).load(loaded);
+$(window).load(app.loaded);
